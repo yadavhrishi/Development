0165d4e6acf2852ac646850a4bfb4222b67a11b6
yadavhrishi <106734319+yadavhrishi@users.noreply.github.com>
2022-08-03 23:19:31 +0530

    Add files via upload

---
+++ LL.java
@@ -0,0 +1,63 @@
+import java.lang.*;
+import java.util.*;
+
+class LL
+{
+	public static void main(String arg[])
+	{
+		LinkedList <String>obj = new LinkedList<String>();
+		obj.add("January");
+		obj.add("February");
+		obj.add("Hello");
+		obj.add("March");
+		obj.add("April");
+		obj.add(2,"New");
+		obj.add("Hello");
+		obj.add("Hello");
+		
+		System.out.println("Array list contains:"+obj);
+		System.out.println("Size is:"+obj.size());
+		
+		System.out.println("Elements are using for loop:");
+		for(int i=0;i<obj.size();i++)
+		{
+			System.out.println(obj.get(i));
+		}	
+		
+		String str;
+		int Cnt=0;
+		for(int i=0;i<obj.size();i++)
+		{
+			str = obj.get(i);
+			if(str.equals("Hello"))
+			{
+				Cnt++;
+			}	
+		}
+		System.out.println("Hello occurs "+Cnt+"times in array list");
+		
+		if(obj.contains("April"))
+		{
+			System.out.println("Element is there");
+		}	
+		
+		int ret = obj.indexOf("April");
+		System.out.println("Element is at:"+ret);
+		
+		System.out.println("Elements at index 3 index :"+obj.get(3));
+		
+		obj.remove(3);
+		obj.remove("April");
+		System.out.println("updated list is"+obj);
+		
+		Iterator iobj = obj.iterator();
+		while(iobj.hasNext())
+		{
+			System.out.println(iobj.next());
+		}	
+		
+		obj.clear();
+		System.out.println("updated list is"+obj);
+
+	}
+}
